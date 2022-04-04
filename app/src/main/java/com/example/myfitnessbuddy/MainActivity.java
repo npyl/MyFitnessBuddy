c7dd77444716b081167b35950f7dc4a798654cd6
vaggroz73 <evaggelosrozos@gmail.com>
2022-04-04 21:24:09 +0300

    [LOGIN/SIGNUP] implement login

--- app/src/main/java/com/example/myfitnessbuddy/MainActivity.java
+++ app/src/main/java/com/example/myfitnessbuddy/MainActivity.java
@@ -1,20 +1,22 @@
 package com.example.myfitnessbuddy;
 
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -59,5 +61,23 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public void onLoginButtonClick(View view) {
+        TextView emailTextField = findViewById(R.id.emailTextField);
+        TextView passwTextField = findViewById(R.id.passwordTextField);
+
+        String email = emailTextField.getText().toString();
+        String passw = passwTextField.getText().toString();
+
+        if (email.isEmpty() || passw.isEmpty())
+            return;
+
+        // TODO: Exception
+
+        Boolean res = LoginAndSignUp.login(email, passw);
+
+        if (res)
+            Log.d("LOGIN", "Successful login!");
     }
 }
