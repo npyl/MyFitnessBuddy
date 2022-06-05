a9fe7dbca8f2812af7282ce9560a237752819c90
Risos13 <--replace-all>
2022-06-05 21:58:54 +0300

    Stubs for ManageDB #4

--- app/src/main/java/com/example/myfitnessbuddy/ManageDB.java
+++ app/src/main/java/com/example/myfitnessbuddy/ManageDB.java
@@ -1,5 +1,54 @@
 package com.example.myfitnessbuddy;
 
-public class ManageDB {
+import java.util.ArrayList;
 
+public class ManageDB {
+    UserInfo                    userInfo;
+    DietPlan                    dietPlan;
+    ExercisePlan                exercisePlan;
+    ArrayList<WorkoutLogEntry>  workoutLog;
+    ArrayList<String>           restaurantList;
+    ArrayList<String>           restaurantsMenus;
+    ArrayList<Food>             meals;
+    ArrayList<String>           professionalsList;
+
+    public UserInfo getUserInfo() {
+        return userInfo;
+    }
+
+    public void addWorkoutLogEntry(WorkoutLogEntry entry)
+    {}
+
+    public ArrayList<WorkoutLogEntry> getWorkoutLog() {
+        return workoutLog;
+    }
+
+    public void updateDietPlan(DietPlan dp)
+    {}
+
+    public void updateExercisesPlan(ExercisePlan ep)
+    {}
+
+    public ArrayList<String> downloadRestaurantList() {
+        return restaurantList;
+    }
+
+    public ArrayList<String> downloadRestaurantsMenus() {
+        return restaurantsMenus;
+    }
+
+    public DietPlan getDietPlan() {
+        return dietPlan;
+    }
+
+    public ExercisePlan getExercisePlan() {
+        return exercisePlan;
+    }
+
+    public void updateMeal(Food meal)
+    {}
+
+    public ArrayList<String> getProfessionalsList() {
+        return professionalsList;
+    }
 }
